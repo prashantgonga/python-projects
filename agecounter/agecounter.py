018bb65d4180cc9b6b4a91e737bce727d9f20c72
Prashant Gonga <prashant.gonga@gmail.com>
2019-05-11 02:17:06 +0800

    Add files via upload

---
+++ agecounter/agecounter.py
@@ -0,0 +1,2 @@
+# https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+# Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
