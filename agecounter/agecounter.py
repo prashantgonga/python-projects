502347c385388dd29fa536ce2886b17f36b65e22
Prashant Gonga <prashant.gonga@gmail.com>
2019-05-11 02:30:04 +0800

    Update agecounter.py

--- agecounter/agecounter.py
+++ agecounter/agecounter.py
@@ -1,2 +1,11 @@
 # https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
-# Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
+# Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+userName = input("Enter your name: ")
+inputAge = input("Enter your current age: ")
+if inputAge is not int :
+    print("Invalid input, enter a number")
+    continue : 
+   
+userAge = int(inputAge)
