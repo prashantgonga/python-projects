abc9c5f028267ecc46b3f7eaeac068dc19bfd0f3
Prashant Gonga <prashant.gonga@gmail.com>
2019-05-11 01:01:23 +0800

    Add files via upload

---
+++ wordcounter/wordcounter.py
@@ -0,0 +1,23 @@
+#Simple program take a user input in a form of a txt file and check which word in the file has been used the most
+#use intro.txt file as a sample text file
+
+fupload = input("Enter your file name: ") #enter the file name
+fhandle = open(fupload) #open the contents of the file
+
+count = dict() #initializing an empty dictionary
+for lines in fhandle :
+    words = lines.split() #splitting the contents of the file in a key value pair
+    for word in words :
+        count[word] = count.get(word, 0) + 1
+        
+mostUsedWord = None
+wordCount = None
+for key, value in count.items() : #key and value are used as a double interation variables for count.items()
+    if mostUsedWord is None or value > wordCount :
+        mostUsedWord = key
+        wordCount = value
+print("The most used word is", mostUsedWord, "and it appeared", wordCount, "times in the text file"  )
+
+
+
+
